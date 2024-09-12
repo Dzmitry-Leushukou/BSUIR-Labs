2ec43dc96f228efedb5246856cd58fac60df32b1
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-09-12 22:04:49 +0300

    Lab update

--- Semester 3/Asm/11.asm
+++ Semester 3/Asm/11.asm
@@ -1,6 +1,21 @@
- org 0
- ldx 5
- ldy 6
+ ldx #5
+ ldy #6
 	
-; 1 way
+; 1 way (use 3 because 2 - offset byte)
+ stx $1
+ sty $3 
+ ldx $3
+ ldy $1
+
+; 2 way
+ xgdx ; (swap x and d)
+ xgdy ; (swap d(x) and y) => d=y y=x 
+ xgdx
+
+; 3 way
+ pshx
+ pshy
+ pulx ; x=y
+ puly ; y=x
  
+
