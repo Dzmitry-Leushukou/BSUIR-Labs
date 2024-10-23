9b2b44aea1fd64e9aa40767cd45aa63bcfcb8cef
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-10-23 14:08:44 +0300

    lab 4 input

--- Semester 3/Assembler/Lab 4/main.asm
+++ Semester 3/Assembler/Lab 4/main.asm
@@ -3,14 +3,13 @@
 
 .data
 a dw 30 dup (0)
-i db 0  
+i dw 0  
 j db 0
 n db 48
-numb db 5 dup (0)  
+h dw 0  
+l dw 0
 sign db 0
-correct db 0
-
-
+correct db 0  
 ;UI
 cin db "Write number (-32768, 32767):", 0Dh, 0Ah, '$'
 wr db "Wrong input", 0Dh, 0Ah, '$' 
@@ -19,47 +18,11 @@ per db 0Dh, 0Ah, '$'
              
 ; include data & stack
 mov ax,@data
-mov ds, ax    
+mov ds, ax
 
-mov si, 0 ; si - size
+; cin
+call input    
 
-input:  
-
-cmp correct, 1
-je call wrong
-    
-cmp i, 30
-jge EOP
-mov j, 0 
-mov ah, 9  
-lea dx, cin
-int 21h 
-mov ah, 1
-mov sign, 0
-mov correct, 0
-int 21h
-cmp al, 45 
-jne read_ch  
-dec j
-call set_sign
-
-
-read_ch:
-
-call clr_cor
-mov ah, 1
-int 21h
-call check 
-cmp correct, 1
-jmp input 
-inc j
-cmp j, 5
-jl read_ch
-
-call endl
-call endl
-inc i
-jmp input
 
 EOP:
 mov ax, 4C00h
@@ -71,8 +34,6 @@ ret
 
 clr_sign:
 mov sign, 0 
-mov numb[0], al
-inc j
 ret
 
 return: 
@@ -80,20 +41,18 @@ ret
 
 check: 
 mov n, 48     
-call clr_cor
-check_loop:
+check_loop: 
 cmp al, n
-je call set_cor
-inc n
+je return 
+inc n 
+cmp n,58
+jge incorrect
 jmp check_loop  
 ret
 
-set_cor:
-mov correct, 1
-ret
-clr_cor:
+incorrect:
 mov correct, 0
-ret    
+ret
   
 wrong:  
 call endl
@@ -101,9 +60,129 @@ mov ah, 9
 lea dx, wr
 int 21h   
 call endl
-ret
+ret 
+
 endl:
 mov ah, 9  
 lea dx, per
 int 21h 
+ret 
+
+prepare_to_iterate:
+mov j,0 ; begin of new number
+mov sign, 0 ; eq to plus
+mov correct, 1;  all correct  
+mov bx, 0 
+mov h, 0
+mov l, 0
+ret   
+
+check_negative:
+mov correct, 0 ;incor
+cmp dx, 0
+jne return
+cmp bx, 32768
+jl return
+mov correct, 1;cor
+ret
+
+check_positive:
+mov correct, 0 ;incor
+cmp dx, 0
+jne return
+cmp bx, 32767
+ja return
+mov correct, 1;cor
+ret
+ 
+input proc  
+cmp i, 30 ; i?30
+jge EOP  
+
+mov ah, 9  
+lea dx, cin
+int 21h 
+
+mov ah, 1  
+
+call prepare_to_iterate
+
+int 21h; read
+
+
+ 
+call set_sign; set sign
+cmp al, 45; if minus 
+je read_ch; start read number         
+
+
+; else first char of number read yet => go to check
+call clr_sign
+jmp check_ch
+
+read_ch:
+
+mov ah, 1
+int 21h
+cmp al, 13
+je comp
+
+check_ch:
+call check 
+
+cmp correct, 0 ; incor
+jne cor_check 
+wrong_check:
+call wrong
+jmp input
+cor_check:
+;else all correct
+sub al,'0'
+mov ah, 0
+mov h, ax
+
+mov ax, 10 
+mul bx
+mov bx, ax
+add bx, h 
+
+to_j:
+inc j; j++
+cmp j, 5 ; if the end of word
+jl read_ch ; read new char
+
+comp:
+cmp sign, 0
+jne negative
+call check_positive 
+  
+cmp correct, 0 ; incor
+jne call iter
+call wrong  
+jmp input
+
+
+negative:
+;sign - minus     
+call to_additional_code
+call check_negative 
+cmp correct, 0 ; incor
+jne call iter
+call wrong  
+jmp input
+
+iter: 
+call endl
+mov a[si], bx
+inc si
+inc si
+inc i ; all good i++     
+jmp input
+
+ret
+ENDP    
+   
+to_additional_code:
+not bx
+add bx, 1
 ret
