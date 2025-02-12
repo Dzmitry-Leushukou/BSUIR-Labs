e2c8a6b7ad2bba53379d812368504a932eff19bd
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2025-02-12 13:00:50 +0300

    Lab 7 and Lab 8 Sem3 C#

---
+++ Semester 3/C# (Programming tools and means)/Lab 7/ClassLibrary/UI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class UI
+    {
+        public static void Found(Int64 ticks, int thread, double res)
+        {
+            Console.WriteLine($"Thread: {thread} Integral was find: {res} by {ticks} ticks");
+        }
+        public static void Process(int per, int thread)
+        {
+            lock (ConsoleLock)
+            {
+                Console.Write($"Thread: {thread} [");
+                for (int i = 0; i <= per / 10 - 1; i++)
+                {
+                    Console.Write("=");
+                }
+                Console.Write(">");
+                for (int i = 0; i < 10 - per / 10; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write($"] {per}%\n");
+            }
+        }
+        private static readonly object ConsoleLock = new object();
+    }
+}
---
+++ Semester 3/C# (Programming tools and means)/Lab 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using LoremNET;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine($"Primary thread: {Thread.CurrentThread.ManagedThreadId}");
+        List<Listener> listeners = new();
+        Progress<string> progress = new(Console.WriteLine);
+        var stream = new MemoryStream();
+        var ss = new StreamService<Listener>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            listeners.Add(new Listener()
+            {
+                Id = listeners.Count(),
+                Name = Lorem.Words(1),
+                CourseCount = Convert.ToInt32(Lorem.Number(1, 256))
+            });
+        }
+
+        var op1 = ss.WriteToStreamAsync(stream, listeners, progress);
+        await Task.Delay(200);
+        var op2 = Task.Run(()=>ss.CopyFromStreamAsync(stream, "data.json", progress));
+        Task.WaitAll(op1, op2);
+        var ans = ss.GetStatisticsAsync("data.json", (listener) => listener.CourseCount > 10);
+
+    }
+}
