17336f62eebb6ff31cee9fb81b89a68e44249c27
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2025-02-19 00:54:26 +0300

    1-2 C#

--- Semester 3/C# (Programming tools and means)/Lab 7/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ Semester 3/C# (Programming tools and means)/Lab 7/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4856b817ba71232a51f70b7b9e61485f96708d30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512c5154bb1f7d260ad8db0bf0fc1c2f5190f150")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
