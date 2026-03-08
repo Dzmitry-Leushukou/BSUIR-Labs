9db101252c14aeea276a021aeda0a1fc57d955d3
Dzmitry Leushukou <dzmitry.levshukov@gmail.com>
2026-03-08 15:37:52 +0300

    LR 4

--- Data models and database management systems/Lab 4/RedisService.py
+++ Data models and database management systems/Lab 4/RedisService.py
@@ -12,21 +12,22 @@ class RedisService:
             decode_responses=True,)
 
     def add_user(self, user:UserDTO):
-        self.client.set(user.user_id, user.to_key_dict())
+        self.client.hset(f"user:{user.user_id}", user.to_key_dict())
         pass
 
     def add_order(self, order:OrderDTO):
-        self.client.set(order.order_id, order.to_key_dict())
+        self.client.hset(f"order:{order.order_id}", order.to_key_dict())
         pass
 
     def add_category(self, category:CategoryDTO):
-        self.client.set(category.category_id, category.to_key_dict())
+        self.client.hset(f"category:{category.category_id}", category.to_key_dict())
         pass
 
     def add_product(self, product:ProductDTO):
-        self.client.set(product.product_id, product.to_key_dict())
+        self.client.hset(f"product:{product.product_id}", product.to_key_dict())
+        self.client.sadd(f"category:{product.category_id}:products", f"product:{product.product_id}")
         pass
 
     def add_order_item(self, order_item:OrderItemDTO):
-        self.client.set(order_item.order_item_id, order_item.to_key_dict())
+        self.client.hset(f"order_item:{order_item.order_item_id}", order_item.to_key_dict())
         pass
